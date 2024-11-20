6b01b380218fe1897a043eafdb485766c6c1d58d
Martin Ceresa <martin.ceresa@imdea.org>
2024-11-20 09:05:47 +0100

    Adding Log games to last game ideas

--- FraudProof/DataStructures/Sequence.lean
+++ FraudProof/DataStructures/Sequence.lean
@@ -13,16 +13,20 @@ def nilSeq {γ : Type} : Sequence 0 γ
 def singleSeq {α : Type} (a : α) : Sequence 1 α
  := Fin.cons a nilSeq
 
-
-
 -- Head
 @[simp]
 def headSeq {α : Type}{ n : Nat } ( seq : Sequence n.succ α) : α
   := seq ⟨ 0 , by simp ⟩
 
+-- Last
 @[simp]
 def lastSeq {α : Type}{n : Nat} (seq : Sequence n.succ α) : α
   := seq $ Fin.last n
+
+-- R Access
+@[simp]
+def getI {α : Type}{n: Nat}(seq : Sequence n α)(i : Nat)(iLTn : i < n) : α
+ := seq ⟨ i , iLTn ⟩
 
 @[simp]
 def tailSeq {α : Type}{n : Nat}: Sequence n.succ α -> Sequence n α
@@ -48,5 +52,13 @@ theorem Fin.snoc_head {α : Type}{ n : Nat }
    ( seq : Sequence n.succ α )(lt : α)
    : (@Fin.snoc n.succ (fun _ => α) seq lt) ⟨ 0 , by simp ⟩ = seq 0
    := by simp [snoc, castLT]
+
+@[simp]
+def takeN {α : Type}{n : Nat}(m : Nat)(mLTn : m < n)(s : Sequence n α) : Sequence m α
+ := fun ⟨ p , plt ⟩ => s ⟨ p , by omega ⟩
+
+@[simp]
+def dropN {α : Type}{n : Nat}(m : Nat)(mLTn : m < n)(s : Sequence n α) : Sequence (n - m) α
+ := fun ⟨ p , pLT ⟩ => s ⟨ p , by omega ⟩
 
 ----------------------------------------
