83f2933c5d6a69b7958c77884ef7f29a8aebdb35
Martin Ceresa <martinceresa@gmail.com>
2025-02-05 15:35:31 +0100

    Knowing Chooser, abtree general game

--- FraudProof/Games/Base/ElemInTree.lean
+++ FraudProof/Games/Base/ElemInTree.lean
@@ -5,6 +5,8 @@ import FraudProof.DataStructures.Sequence
 
 import Mathlib.Data.Fin.Tuple.Basic
 
+import FraudProof.DataStructures.Hash -- Hash Stuff
+
 ----------------------------------------
 -- * Element in Tree basic game definitions Linear game and simple games playing
 -- with the idea of needing to have the element in question or not. If we do not
@@ -335,3 +337,118 @@ theorem winning_reveler_wins_forward {ℍ : Type}
       | Continue s =>
        simp; rw [HP] at wProp; simp at wProp
        exact Hind ⟨ Fin.tail da.data, (p.1 , da.mtree.2)⟩ (Fin.tail prop) wProp.2 (Fin.tail ch)
+
+--
+--
+-- * Chooser Conditions
+
+-- I am sure I defined this somewhere else
+def last_step {ℍ : Type}[BEq ℍ][m : HashMagma ℍ ]
+  (side : SkElem)(res : ℍ) (data : ℍ × ℍ)  : Winner
+  := condWProp $ op_side side data.1 res == data.2
+
+def knowing {ℍ : Type}[BEq ℍ][HashMagma ℍ]
+  ( skl : ABTree SkElem Unit )( know : ABTree ℍ ℍ ) (i o : ℍ) : Prop
+  := match skl, know with
+     -- Cases
+     | .leaf sk, .leaf h => op_side sk i h = o
+     | .node _ al ar , .node mid kl kr =>
+       knowing al kl i mid ∧ knowing ar kr mid o
+     -- Same structure
+     | _ , _ => False
+
+def input_prop {ℍ : Type} (data : ABTree (Option ℍ) (Option ℍ))(i : ℍ)
+  : Prop
+  := match data with
+    | .node _ bl _ => input_prop bl i
+    | .leaf (.some h) => h = i
+    | .leaf none => False
+
+theorem range_chooser_wins {ℍ : Type}
+    [BEq ℍ][LawfulBEq ℍ]
+    [HashMagma ℍ][hash_props : SLawFulHash ℍ]
+    (comp_skeleton : ABTree SkElem Unit)
+    (input_rev input_ch: ℍ)(output : ℍ)
+    -- current da is {comp_skeleton , (input_rev , output) }
+    (hneq : ¬ input_rev = input_ch)
+    -- Reveler
+    (reveler : ABTree (Option ℍ) (Option ℍ))
+    -- Chooser
+    (chooser : ABTree ℍ ℍ)
+    -- Computation is fold plus invariants.
+    (chooser_wise : knowing comp_skeleton chooser input_ch output)
+    : simp_tree (fun (l,r) mid => ((l,mid),(mid,r)))
+      last_step
+      -- No mid challenges
+      (fun _ _ _ => Player.Proposer)
+      { data:= comp_skeleton , res := (input_rev , output) }
+      reveler
+      (gen_to_fun_chooser (ABTree.map .some .some chooser))
+      = Player.Chooser
+    := by
+  revert chooser_wise chooser reveler hneq input_rev input_ch output
+  induction comp_skeleton with
+  | leaf sk =>
+    intros in_rev in_ch out hneq rev cho kcho
+    cases HRev : rev with
+    | node _ _ _ => simp [simp_tree]
+    | leaf p =>
+      cases HP : p with
+      | none => simp [simp_tree]
+      | some proposed =>
+        simp [gen_to_fun_chooser, simp_tree]
+        -- unfold knowing at kcho
+        cases cho with
+        | node _ _ _ =>
+          simp [knowing] at kcho
+        | leaf ch =>
+          simp [knowing] at kcho
+          simp [last_step, condWProp]
+          rw [<- kcho]
+          cases sk with
+          | inl _ =>
+            simp at *
+            apply hash_props.neqLeft
+            assumption
+          | inr _ =>
+            simp at *
+            apply hash_props.neqRight
+            assumption
+  | node _ game_left game_right HIndL HIndR =>
+    intros in_rev in_ch out hneq rev cho kcho
+    simp
+    cases rev with
+    | leaf _ => simp [simp_tree]
+    | node may_mid rev_l rev_r =>
+      cases may_mid with
+      | none => simp [simp_tree]
+      | some mid =>
+        simp [simp_tree]
+        cases cho with
+        | leaf _ => simp [knowing] at kcho
+        | node cho_mid ch_l ch_r =>
+          simp [gen_to_fun_chooser]
+          split
+          case h_1 x heq =>
+            rw [ite_eq_iff] at heq
+            simp at heq
+          case h_2 x heq =>
+           simp at heq
+           apply HIndL
+           --
+           exact hneq
+           --
+           simp [knowing] at kcho
+           rw [<- heq]
+           exact kcho.1
+          case h_3 x heq =>
+           simp at heq
+           have hind := HIndR mid cho_mid out (by intro ha; apply heq; rw [ha])
+           -- apply HIndR
+           apply hind
+           --
+           simp [knowing] at kcho
+           exact kcho.2
+          case h_4 x heq =>
+            rw [ite_eq_iff] at heq
+            simp at heq
