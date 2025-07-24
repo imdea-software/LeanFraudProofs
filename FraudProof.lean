a455beedad63994073de28181c916aa3d525fc08
Martin Ceresa <martinceresa@gmail.com>
2025-07-24 15:48:29 +0200

    Finished History L2 auxiliary lemmas

--- FraudProof.lean
+++ FraudProof.lean
@@ -15,4 +15,4 @@ import FraudProof.Games.PathToTreeGames
 import FraudProof.Games.ReverseLinearGame
 import FraudProof.Games.Simultaneous
 import FraudProof.L2
--- import FraudProof.History_L2 -- WIP
+import FraudProof.History_L2 -- WIP
