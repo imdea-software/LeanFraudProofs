fe60a83cb4928fc6dc67a2320b0ae2a3e1b7814a
Martin Ceresa <martinceresa@gmail.com>
2025-08-22 11:44:28 -0300

    Paths are Accesses

--- FraudProof.lean
+++ FraudProof.lean
@@ -9,10 +9,10 @@ import FraudProof.Games.ElemInTree
 import FraudProof.Games.FMBC
 import FraudProof.Games.FromBtoMTree
 import FraudProof.Games.GameDef
--- import FraudProof.Games.GenericTree
--- import FraudProof.Games.LogProof
+import FraudProof.Games.GenericTree
+import FraudProof.Games.LogProof
 import FraudProof.Games.PathToTreeGames
 import FraudProof.Games.ReverseLinearGame
 import FraudProof.Games.Simultaneous
+import FraudProof.History_L2
 import FraudProof.L2
-import FraudProof.History_L2 -- WIP
